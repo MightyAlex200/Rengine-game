19699c22cc0a1e0500559091a118d6f2eaa60f29
mightyalex200 <quantumtraveling@gmail.com>
2017-01-28 15:21:02 -0500

    did a thing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl;
 
 using namespace sf;
 
 int main(){
 
   RenderWindow mainwindow(VideoMode(640,480), "Rengine");
+
+  std::vector<RectangleShape*> objects;
+
+  RectangleShape* player = new RectangleShape(Vector2f(50,50));
+  float walkspeed = 0.02;
+
+  objects.push_back(player);
 
   while(mainwindow.isOpen()){
     Event e;
@@ -19,8 +30,25 @@ int main(){
       }
     }
 
+    if(Keyboard::isKeyPressed(Keyboard::Right)){
+      player->move(walkspeed,0);
+    }
+    if(Keyboard::isKeyPressed(Keyboard::Left)){
+      player->move(-walkspeed,0);
+    }
+    if(Keyboard::isKeyPressed(Keyboard::Up)){
+      player->move(0,-walkspeed);
+    }
+    if(Keyboard::isKeyPressed(Keyboard::Down)){
+      player->move(0,walkspeed);
+    }
+
     mainwindow.clear();
-    
+
+    for(RectangleShape* s : objects){
+      mainwindow.draw(*s);
+    }
+
     mainwindow.display();
 
   }
