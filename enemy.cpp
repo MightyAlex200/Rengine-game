07c5b6c3ab1b1b8ee1bdca5b6d37f5b2d8699f7b
mightyalex200 <quantumtraveling@gmail.com>
2017-01-29 17:46:30 -0500

    made enemies die after they go too far down

--- enemy.cpp
+++ enemy.cpp
@@ -13,6 +13,9 @@ void Enemy::draw(sf::RenderWindow* window){
 
 void Enemy::update(std::vector<Bullet*> bullets, std::vector<Enemy*>& enemies){
   shape->move(0,speed);
+  if(shape->getPosition().y >= 480){
+    enemies.erase(std::remove(enemies.begin(), enemies.end(), this), enemies.end());
+  }
   for(Bullet* b : bullets){
     if(shape->getGlobalBounds().intersects(b->shape->getGlobalBounds())){ // If colliding with bullet
       enemies.erase(std::remove(enemies.begin(), enemies.end(), this), enemies.end());
